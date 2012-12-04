da4fbdb2c247b2fed104e3db90a9d70204fe0da0
L42y <423300@gmail.com>
2012-12-04 12:57:52 +0800

    Removed smart-forward related keybindings

--- keybindings.el
+++ keybindings.el
@@ -3,17 +3,6 @@
 (global-set-key (kbd "C-c g") 'comment-or-uncomment-region)
 
 
-;;; smart-forward
-(global-set-key (kbd "M-<up>") 'smart-up)
-(global-set-key (kbd "M-<down>") 'smart-down)
-(global-set-key (kbd "M-<left>") 'smart-backward)
-(global-set-key (kbd "M-<right>") 'smart-forward)
-(global-set-key (kbd "C-M-p") 'smart-up)
-(global-set-key (kbd "C-M-n") 'smart-down)
-(global-set-key (kbd "C-M-b") 'smart-backward)
-(global-set-key (kbd "C-M-f") 'smart-forward)
-
-
 ;;; helm
 (global-set-key (kbd "C-x C-a") 'helm-ack)
 (global-set-key (kbd "C-x C-y") 'helm-show-kill-ring)
