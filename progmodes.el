ba53a7d1b9929a3ddf1c18e64ccd6834ae10ea0f
L42y <423300@gmail.com>
2013-01-25 15:07:03 +0800

    Enable flycheck for all prog modes

--- progmodes.el
+++ progmodes.el
@@ -27,16 +27,7 @@
 
 
 ;;; flycheck
-(dolist (hook '(js-mode-hook
-                js2-mode-hook
-                css-mode-hook
-                sgml-mode-hook
-                haml-mode-hook
-                sass-mode-hook
-                ruby-mode-hook
-                python-mode-hook
-                emacs-lisp-mode-hook))
-  (add-hook hook (lambda () (flycheck-mode-on))))
+(add-hook 'prog-mode-hook 'flycheck-mode)
 (setq flycheck-error-indicator nil)
 
 
