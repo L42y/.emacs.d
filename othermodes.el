32478b41670e18677b69dd4c0b500f6340a85f21
L42y <423300@gmail.com>
2013-03-31 19:45:58 +0800

    Separate built-in and third-party packages

--- othermodes.el
+++ othermodes.el
@@ -1,3 +1,31 @@
+;; built-in packages
+;;; make buffer name uniquify
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward
+      uniquify-separator "@")
+
+
+;;; desktop
+(desktop-save-mode 1)
+(setq desktop-path '("~/.emacs.d/"))
+(setq desktop-base-file-name ".emacs.desktop")
+(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+
+
+;;; use ido for minibuffer completion
+(ido-mode t)
+(setq ido-show-dot-for-dired t)
+(setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
+
+
+;;; org-mode awesomeness
+(require 'org)
+(add-hook 'org-mode-hook
+          (lambda ()
+            (toggle-truncate-lines)))
+
+
+;; third-party packages
 ;;; ag.el
 (setq ag-arguments (list
                     "--column"
@@ -12,18 +40,9 @@
 (setq httpd-root "~/.emacs.d/el-get/skewer-mode")
 (httpd-start)
 
-
-;;; make buffer name uniquify
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward
-      uniquify-separator "@")
-
-
-;;; desktop
-(desktop-save-mode 1)
-(setq desktop-path '("~/.emacs.d/"))
-(setq desktop-base-file-name ".emacs.desktop")
-(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+(add-hook 'skewer-css-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook 'skewer-css-eval-buffer nil t)))
 
 
 ;;; helm awesomeness
@@ -45,26 +64,12 @@
 (projectile-global-mode)
 
 
-;;; use ido for minibuffer completion
-(ido-mode t)
-(setq ido-show-dot-for-dired t)
-(setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
-
-
 ;;; ibuffer-vc
 (add-hook 'ibuffer-hook
           (lambda ()
             (ibuffer-vc-set-filter-groups-by-vc-root)
             (unless (eq ibuffer-sorting-mode 'alphabetic)
               (ibuffer-do-sort-by-alphabetic))))
-
-
-;;; org-mode awesomeness
-(require 'org)
-(define-key global-map "\C-ca" 'org-agenda)
-(add-hook 'org-mode-hook
-          (lambda ()
-            (toggle-truncate-lines)))
 
 
 ;;; wdired
