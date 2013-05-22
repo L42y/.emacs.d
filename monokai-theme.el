91a0164b1363c5ccdad7e211840599fa2e12da3c
L42y <423300@gmail.com>
2013-05-22 17:52:10 +0800

    Update monokai-theme to matches original one

--- monokai-theme.el
+++ monokai-theme.el
@@ -10,11 +10,11 @@
 (let ((class '((class color) (min-colors 89))))
   (custom-theme-set-faces
    'monokai
-   `(default ((,class (:background "#272821" :foreground "#F8F8F2"))))
-   `(cursor ((,class (:background "#DAD085" :foreground "#656565"))))
+   `(default ((,class (:background "#272822" :foreground "#F8F8F0"))))
+   `(cursor ((,class (:background "#E6DB74" :foreground "#656565"))))
    ;; Mode line faces
    `(mode-line ((,class (:box nil
-                         :background "white" :foreground "black"))))
+                         :background "#E6DB74" :foreground "#656565"))))
    `(mode-line-inactive ((,class (:box nil))))
    ;; hl-line face
    `(hl-line ((,class (:background "#1A1A1A"))))
@@ -22,20 +22,18 @@
    `(fringe ((,class (:background "#222"))))
    `(vertical-border ((,class (:foreground "#272821"))))
    ;; region face
-   `(region ((,class (:background "#6DC5F1"))))
+   `(region ((,class (:background "#49483E"))))
    ;; Font lock faces
-   `(font-lock-builtin-face ((,class (:foreground "#A6E22A"))))
-   `(font-lock-comment-face ((,class (:italic t :foreground "#75715D"))))
-   `(font-lock-constant-face ((,class (:foreground "#A6E22A"))))
-   `(font-lock-function-name-face ((,class (:italic t :foreground "#F1266F"))))
-   `(font-lock-keyword-face ((,class (:foreground "#66D9EF"))))
-   `(font-lock-string-face ((,class (:foreground "#DFD874"))))
-   `(font-lock-type-face ((,class (:foreground "#89BDFF"))))
-   `(font-lock-variable-name-face ((,class (:foreground "#A6E22A"))))
-   `(font-lock-warning-face ((,class (:foreground "#FD5FF1"))))
-
-   `(flymake-errline ((,class (:foreground "#F1266F" :underline t))))
-   `(flymake-warnline ((,class (:foreground "#FD5FF1" :underline t))))))
+   `(font-lock-builtin-face ((,class (:foreground "#A6E22E"))))
+   `(font-lock-comment-face ((,class (:italic t :foreground "#75715E"))))
+   `(font-lock-comment-delimiter-face ((,class (:italic nil :bold t))))
+   `(font-lock-constant-face ((,class (:foreground "#FD971F"))))
+   `(font-lock-function-name-face ((,class (:italic t :foreground "#66D9EF"))))
+   `(font-lock-keyword-face ((,class (:foreground "#F92672"))))
+   `(font-lock-string-face ((,class (:foreground "#E6DB74"))))
+   `(font-lock-type-face ((,class (:foreground "#66D9EF"))))
+   `(font-lock-variable-name-face ((,class (:foreground "#A6E22E"))))
+   `(font-lock-warning-face ((,class (:foreground "#AE81FF"))))))
 
 (provide-theme 'monokai)
 
