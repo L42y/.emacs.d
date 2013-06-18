7723d27136d20de5dded22816276549698016be5
L42y <423300@gmail.com>
2013-06-18 12:14:23 +0800

    Add list of package should be installed

--- pkgs.el
+++ pkgs.el
@@ -11,5 +11,43 @@
 (package-initialize)
 
 
+;;; install missing packages
+(defvar my-packages '(ag
+                      ace-jump-mode
+                      angular-snippets
+                      auto-complete
+                      better-defaults
+                      diff-hl
+                      exec-path-from-shell
+                      expand-region
+                      flycheck
+                      helm
+                      helm-projectile
+                      ibuffer-vc
+                      jedi
+                      js2-mode
+                      magit
+                      markdown-mode
+                      multiple-cursors
+                      nrepl
+                      org
+                      paredit
+                      projectile
+                      rainbow-delimiters
+                      scss-mode
+                      simplezen
+                      skewer-mode
+                      smartparens
+                      smex
+                      tagedit
+                      yasnippet))
+
+(dolist (package my-packages)
+  (unless (package-installed-p package)
+    (package-refresh-contents)
+    (package-install package)))
+
+
+
 (provide 'pkgs)
 ;;; pkgs.el ends here
