2ad527b2c7e942ae038be4bf68aef64fcecc9936
L42y <423300@gmail.com>
2013-08-11 03:52:59 +0800

    init: homemade™

--- init.el
+++ init.el
@@ -5,7 +5,6 @@
 ;; Author: L42y <423300@gmail.com>
 ;; URL: https://github.com/L42y/.emacs.d
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
-;; Based on dimitri's emacs-kicker: https://github.com/dimitri/emacs-kicker
 ;;
 ;; This file is NOT part of GNU Emacs.
 
