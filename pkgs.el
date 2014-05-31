6dcf477910deea5804db01b809557e707b3a7eb6
L42y <423300@gmail.com>
2014-06-01 04:43:48 +0800

    pkg: style

--- pkgs.el
+++ pkgs.el
@@ -9,8 +9,7 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("org" . "http://orgmode.org/elpa/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")
-        ))
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 
