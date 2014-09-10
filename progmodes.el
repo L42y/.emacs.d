a6ad69105c8feb21292207cacb8534f1c93e2909
L42y <423300@gmail.com>
2014-09-10 19:51:20 +0800

    js: js-doc key bindings

--- progmodes.el
+++ progmodes.el
@@ -73,6 +73,11 @@
               js2-basic-offset 2)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(add-hook 'js2-mode-hook
+  '(lambda ()
+     (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
+     (define-key js2-mode-map "@" 'js-doc-insert-tag)))
+
 
 ;;; auto insert
 (require 'autoinsert)
