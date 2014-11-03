9d6ebfb95f90a3a8d29093029a2a991febca7f7e
L42y <423300@gmail.com>
2014-11-03 11:04:05 +0800

    pkg: re-indent

--- pkgs.el
+++ pkgs.el
@@ -7,63 +7,61 @@
 ;;; builtin package
 (require 'package)
 (setq package-archives
-  '(
-     ("gnu" . "https://elpa.gnu.org/packages/")
-     ("org" . "http://orgmode.org/elpa/")
-     ("melpa" . "http://melpa.org/packages/")))
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")
+        ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
 
 ;;; install missing packages
-(defvar my-packages '(
-                       ag
-                       ace-jump-mode
-                       angular-snippets
-                       auto-complete
-                       better-defaults
-                       color-identifiers-mode
-                       diff-hl
-                       editorconfig
-                       emmet-mode
-                       exec-path-from-shell
-                       expand-region
-                       flatui-theme
-                       flycheck
-                       flycheck-tip
-                       gitattributes-mode
-                       gitconfig-mode
-                       gitignore-mode
-                       go-mode
-                       helm
-                       helm-open-github
-                       helm-projectile
-                       htmlize
-                       ibuffer-vc
-                       jedi
-                       js-doc
-                       js2-mode
-                       json-mode
-                       magit
-                       markdown-mode
-                       monokai-theme
-                       multiple-cursors
-                       nginx-mode
-                       org
-                       org-plus-contrib
-                       paredit
-                       pip-requirements
-                       pkgbuild-mode
-                       projectile
-                       rainbow-delimiters
-                       rainbow-mode
-                       smartparens
-                       smex
-                       string-edit
-                       tagedit
-                       tern
-                       tern-auto-complete
-                       virtualenvwrapper
-                       yasnippet))
+(defvar my-packages '(ag
+                      ace-jump-mode
+                      angular-snippets
+                      auto-complete
+                      better-defaults
+                      color-identifiers-mode
+                      diff-hl
+                      editorconfig
+                      emmet-mode
+                      exec-path-from-shell
+                      expand-region
+                      flatui-theme
+                      flycheck
+                      flycheck-tip
+                      gitattributes-mode
+                      gitconfig-mode
+                      gitignore-mode
+                      go-mode
+                      helm
+                      helm-open-github
+                      helm-projectile
+                      htmlize
+                      ibuffer-vc
+                      jedi
+                      js-doc
+                      js2-mode
+                      json-mode
+                      magit
+                      markdown-mode
+                      monokai-theme
+                      multiple-cursors
+                      nginx-mode
+                      org
+                      org-plus-contrib
+                      paredit
+                      pip-requirements
+                      pkgbuild-mode
+                      projectile
+                      rainbow-delimiters
+                      rainbow-mode
+                      smartparens
+                      smex
+                      string-edit
+                      tagedit
+                      tern
+                      tern-auto-complete
+                      virtualenvwrapper
+                      yasnippet))
 
 (dolist (package my-packages)
   (unless (package-installed-p package)
