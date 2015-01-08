c633cbae626c1e8bf849481fea43a1ddd39abe0c
L42y <423300@gmail.com>
2015-01-08 13:55:31 +0800

    (use-package multiple-cursors)

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -8,13 +8,6 @@
 (global-set-key (kbd "C-x C-k") 'save-buffers-kill-emacs)
 
 
-;;; multiple-cursors
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-
 ;;; string-edit
 (global-set-key (kbd "C-c i") 'string-edit-at-point)
 
--- lisp/othermodes.el
+++ lisp/othermodes.el
@@ -261,5 +261,13 @@
   :bind ("C-x <RET>" . smex))
 
 
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)))
+
+
 (provide 'othermodes)
 ;;; othermodes.el ends here
