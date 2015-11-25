f9475cec2700c37ee7ed0d950b3638d035f4bcc6
L42y <423300@gmail.com>
2015-11-25 12:00:04 +0800

    do not enable tagedit on web-mode

--- lisp/othermodes.el
+++ lisp/othermodes.el
@@ -106,7 +106,7 @@
 
 (use-package tagedit
   :ensure t
-  :init (dolist (hook '(sgml-mode-hook web-mode-hook js2-jsx-mode-hook))
+  :init (dolist (hook '(sgml-mode-hook js2-jsx-mode-hook))
           (add-hook hook #'tagedit-mode))
   :config (progn
             (tagedit-add-paredit-like-keybindings)
