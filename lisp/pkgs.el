774e2c6970979bc1fbc99ae48416877011dc646c
L42y <423300@gmail.com>
2016-10-10 15:48:53 +0800

    remove el-get

--- lisp/pkgs.el
+++ lisp/pkgs.el
@@ -12,20 +12,4 @@
 (require 'use-package)
 
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil t)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://github.com/dimitri/el-get/raw/master/el-get-install.el")
-    (end-of-buffer)
-    (eval-print-last-sexp)))
-
-(setq
- my:el-get-packages
- '(el-get))
-
-(el-get 'sync my:el-get-packages)
-
-
 (provide 'pkgs)
